e62a2cda16074f239cc96b287fce54141fa2a56f
Vayom PC <korefan639@gmail.com>
2024-04-06 04:48:00 +0300

    add class CompletedTask for viewing completed tasks

--- todo_api/views.py
+++ todo_api/views.py
@@ -1,32 +1,20 @@
-from django.contrib.auth.models import User
-from django.db.models import QuerySet
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-import rest_framework.permissions as permissions
+"""
+Module containing views for handling tasks in the todo application.
 
+This module defines views for managing tasks, including creating, updating, deleting, and retrieving tasks.
+It also provides views for retrieving completed tasks for the authenticated user.
+
+Classes:
+    TodoViewSet: A viewset for handling CRUD operations on tasks belonging to the authenticated user.
+    CompletedTask: A view for retrieving a list of completed tasks for the current user.
+"""
+
+from django.db.models import QuerySet
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 from todo.models import Task
 from todo_api.permissions import IsOwner
-from todo_api.serializers import TaskSerializer, UserSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    A viewset for handling User objects.
-
-    Provides CRUD operations for User objects, including list, retrieve, create,
-    update, and delete.
-
-    Attributes:
-        serializer_class (UserSerializer): The serializer class used for serializing User objects.
-        queryset (QuerySet): The queryset representing all User objects, ordered by date joined.
-        permission_classes (list): The list of permission classes applied to this viewset.
-            By default, only users with appropriate permissions are allowed to perform operations.
-
-    """
-    serializer_class = UserSerializer
-    queryset = User.objects.all().order_by("-date_joined")
-    permission_classes = [permissions.IsAdminUser]
+from todo_api.serializers import TaskSerializer
 
 
 class TodoViewSet(viewsets.ModelViewSet):
@@ -57,3 +45,34 @@ class TodoViewSet(viewsets.ModelViewSet):
         """
         user = self.request.user
         return Task.objects.filter(user=user)
+
+
+class CompletedTask(generics.ListAPIView):
+    """
+        View that returns a list of completed tasks for the current user.
+
+        Attributes:
+            serializer_class (SerializerClass): The serializer used to serialize task data into JSON format.
+            permission_classes (list): A list of permission classes determining whether the current user has access to
+            this view.
+
+        Methods:
+            get_queryset(self) -> QuerySet[Task]: Returns a queryset of tasks to be displayed in the view.
+                Filters tasks based on completion status and the user making the request.
+
+        Example Usage:
+            Suppose you have an API for managing tasks. You can use this view to get a list of all completed tasks for
+            the current user.
+    """
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet[Task]:
+        """
+            Return the queryset of tasks filtered by completion status and the current user.
+
+            Returns:
+                QuerySet[Task]: A queryset of tasks filtered by completion status and the current user.
+
+        """
+        return Task.objects.filter(completed=True, user=self.request.user)
