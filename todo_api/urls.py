29213d5fa91cf4277b2118b6341fcdeb1cc95a0f
Vayom PC <korefan639@gmail.com>
2024-04-06 04:48:17 +0300

    add url for viewing completed tasks

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -3,14 +3,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 
-from todo_api.views import TodoViewSet, UserViewSet
+from todo_api.views import TodoViewSet, CompletedTask
 
 app_name = 'todo_api'
 
 router = DefaultRouter()
 router.register(r'tasks', TodoViewSet, basename='task')
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('completed_tasks/', CompletedTask.as_view(), name='completed_tasks'),
 ]
